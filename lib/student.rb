e5cbb5a11b5ac34573d8962646d2532b4f31c02f
Tam Vu <vutam_a@hotmail.com>
2019-09-25 02:20:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,8 +63,11 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
+      LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, self.name)
+    DB[:conn].execute(sql, name).map |row|
+      self.new_from_db(row)
+    end.first
   end 
 end
