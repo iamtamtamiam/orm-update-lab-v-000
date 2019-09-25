6ab52bb582ae4de058f077af8161e165150ba185
Tam Vu <vutam_a@hotmail.com>
2019-09-25 02:22:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,8 +66,9 @@ class Student
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, name).map |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
+    end
   end 
   
 end
